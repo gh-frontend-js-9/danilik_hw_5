139244331d19a21423bd54137b630f548778c387
Yaroslav Danilik <yarosalvdaniliK@gmail.com>
2019-11-13 14:34:41 +0200

    refactoring RWD and burger menu

--- homework_hw_5/src/scripts/main.js
+++ homework_hw_5/src/scripts/main.js
@@ -1,3 +1,16 @@
+
 $(document).ready(function() {
+
+  let burger = $('.burger');
+  let menu = $('.m-main-nav__list');
+  let close = $('.close-menu');
+
+  burger.on('click', function(event) {
+    menu.toggleClass('m-main-nav__list--active');
+});
+
+  close.on('click', function(event) {
+    menu.toggleClass('m-main-nav__list--active');
+});
 
 });
